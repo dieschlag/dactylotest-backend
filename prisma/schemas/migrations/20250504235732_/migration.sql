830fd315d2336475510c5a8a213f020a9d147788
dieschlag <dischlermateo@outlook.fr>
2025-05-05 07:21:26 +0700

    added schemas for Preformance and Text

---
+++ prisma/schemas/migrations/20250504235732_/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" INTEGER,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_by" INTEGER,
+    "deleted_at" TIMESTAMP(3),
+    "deleted_by" INTEGER,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
