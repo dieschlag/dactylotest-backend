938ab6bbac33ce54c34016c2bc07072a5cd65fff
dieschlag <dischlermateo@outlook.fr>
2025-05-15 23:10:15 +0700

    added fogotten function call

--- prisma/seeders/database.seeder.ts
+++ prisma/seeders/database.seeder.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { userFactory } from "../factories/user.factory";
+import { quoteFactory } from "../factories/quote.seeder";
 
 const prisma = new PrismaClient();
 
 export async function seed() {
   try {
     await userFactory();
+    await quoteFactory();
   } catch (e) {
     console.error(e);
     process.exit(1);
